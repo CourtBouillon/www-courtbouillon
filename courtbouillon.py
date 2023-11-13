a3f95e9d5d831f3e097cd090027dffa2f7a8d17c
Guillaume Ayoub <guillaume@courtbouillon.org>
2023-11-13 22:36:27 +0100

    Clean code

--- courtbouillon.py
+++ courtbouillon.py
@@ -6,53 +6,57 @@ from flask import Flask, Response, render_template
 from flask_frozen import Freezer
 from pygments import formatters, highlight, lexers
 
-app = Flask(__name__, static_url_path='/static')
+app = Flask(__name__)
+TEMPLATES = Path(app.root_path) / app.template_folder
 
 
 def list_articles():
+    articles_path = TEMPLATES / 'articles'
+    if not articles_path.is_dir():
+        return {}
+
     articles = {}
-    articles_path = Path('templates/articles')
-    if articles_path.is_dir():
-        for article in articles_path.iterdir():
-            if article.is_file() and not article.name.startswith('_'):
-                content = (articles_path / article.name).read_text()
-                introduction = (
-                    content.split('<header>')[1].split('</header>')[0])
-                title = introduction.split('<h2>')[1].split('</h2>')[0]
-                description = introduction.split('</aside>')[1]
-                date = format_datetime(datetime.strptime(
-                    introduction.split('datetime="')[1].split('"')[0],
-                    '%Y-%m-%d'))
-                article_id = article.name.split('-')[0]
-                articles[article_id] = {
-                    'filename': article.name.split('.html')[0],
-                    'introduction': introduction.replace('h2>', 'h3>'),
-                    'title': title,
-                    'description': description,
-                    'date': date}
+    for article in articles_path.iterdir():
+        if not article.is_file() or article.name.startswith('_'):
+            continue
+        content = (articles_path / article.name).read_text()
+        introduction = content.split('<header>')[1].split('</header>')[0]
+        title = introduction.split('<h2>')[1].split('</h2>')[0]
+        description = introduction.split('</aside>')[1]
+        date_string = introduction.split('datetime="')[1].split('"')[0]
+        date = format_datetime(datetime.strptime(date_string, '%Y-%m-%d'))
+        article_id = article.name.split('-')[0]
+        articles[article_id] = {
+            'filename': article.name.split('.html')[0],
+            'introduction': introduction.replace('h2>', 'h3>'),
+            'title': title,
+            'description': description,
+            'date': date,
+        }
+
     return dict(sorted(articles.items(), reverse=True))
 
 
-@app.route('/blog-articles')
-@app.route('/blog/<article>')
+@app.route('/blog-articles/')
+@app.route('/blog/<article>/')
 def blog(article=None):
     if article is not None:
         article_object = list_articles()[article.split('-')[0]]
-        return render_template(
-            f'articles/{article}.html.jinja2', page='article',
-            **article_object)
+        template = f'articles/{article}.html.jinja2'
+        return render_template(template, page='article', **article_object)
     return render_template('blog.html.jinja2', articles=list_articles())
 
 
 @app.route('/')
-@app.route('/<path:page>')
+@app.route('/<path:page>/')
 def page(page='index'):
     return render_template(f'{page}.html.jinja2', page=page)
 
 
 @app.route('/blog.rss')
 def rss():
-    return render_template('blog.rss.jinja2', articles=list_articles())
+    rss = render_template('blog.rss.jinja2', articles=list_articles())
+    return Response(rss, mimetype='application/rss+xml')
 
 
 @app.route('/robots.txt')
@@ -62,10 +66,9 @@ def robots():
 
 @app.template_filter()
 def pygmentize(filename):
-    content = (Path('templates/snippets') / filename).read_text()
-    return highlight(
-        content, lexers.get_lexer_for_filename(filename),
-        formatters.HtmlFormatter())
+    content = (TEMPLATES / 'snippets' / filename).read_text()
+    lexer = lexers.get_lexer_for_filename(filename)
+    return highlight(content, lexer, formatters.HtmlFormatter())
 
 
 @app.cli.command('freeze')
